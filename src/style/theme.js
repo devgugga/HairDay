52087453ba97c42f82b65c66db08fcde586e65cf
devgugga <gustavogomes90104@gmail.com>
2024-11-29 22:57:46 -0300

    :lipstick: Tema da Aplicação & Estilos Globais
    Neste commit foi criado o tema da aplicação juntamente com os estilos gerais.

---
+++ src/style/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    primary: {
+      yellow_light: "#DBC170",
+      yellow: "#B8952E",
+      yellow_dark: "#846F2E",
+    },
+    gray: {
+      100: "#F5F4F5",
+      200: "#B2AFB6",
+      300: "#98959D",
+      400: "#7A767F",
+      500: "#3E3C41",
+      600: "#2E2C30",
+      700: "#232225",
+      800: "#19181B",
+      900: "#050505",
+    },
+  },
+};
